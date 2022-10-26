7dd74c21a5eee2bb10386799ff3f526947e07dce
Min Kookie <min03041999@gmail.com>
2022-10-26 08:04:57 +0700

    Finished page NotFound

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import "bootstrap/dist/css/bootstrap.css";
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import AdminPage from "./Page/Admin/AdminPage";
+import NotFound from "./Page/NotFound";
 import "./App.css";
 
 function App() {
   return (
     <Switch>
       <Route path="/admin" component={AdminPage} />
+      <Route component={NotFound} />
     </Switch>
   );
 }
---
+++ src/Page/NotFound/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NotFound = () => {
+  return <div>Không tìm thấy page mà bạn muốn tìm!</div>;
+};
+
+export default NotFound;
