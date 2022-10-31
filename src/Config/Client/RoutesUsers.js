74bf7464ebd3b31d6530027f655055eb5111a138
Min Kookie <min03041999@gmail.com>
2022-10-31 16:14:57 +0700

    Fix Header of Client

--- src/Config/Client/RoutesUsers.js
+++ src/Config/Client/RoutesUsers.js
@@ -2,15 +2,17 @@ import React from "react";
 import { Route, Switch, useRouteMatch } from "react-router-dom";
 import Animal from "../../component/News";
 import Customer from "../../component/Customer";
+import Contact from "../../component/Contact";
 import NotFound from "../../component/NotFound";
 
 const RoutesClient = () => {
-  const { path } = useRouteMatch();
+  // const { path } = useRouteMatch();
 
   return (
     <Switch>
-      <Route path={`${path}`} component={Animal} exact />
-      <Route path={`${path}customer`} component={Customer} />
+      <Route path={"/"} component={Animal} exact />
+      <Route path={"/customer"} component={Customer} />
+      <Route path={"/contact"} component={Contact} />
       <Route component={NotFound} />
     </Switch>
   );
